a8b82b56136004d440cc93eb1c41ab3d0ac1a149
Artem Zavtur <qwerasss1000@gmail.com>
2022-12-03 15:43:53 +0200

    still working

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.criminalintent
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import java.util.*
 
 class CrimeListViewModel : ViewModel() {
@@ -8,15 +10,17 @@ class CrimeListViewModel : ViewModel() {
     val crimes = mutableListOf<Crime>()
 
     init {
-        for (i in 0 until 100) {
-            val crime = Crime(
-                id = UUID.randomUUID(),
-                title ="Crime #$i",
-                date = Date(),
-                isSolved = i % 2 == 0
-            )
+        viewModelScope.launch {
+            for (i in 0 until 100) {
+                val crime = Crime(
+                    id = UUID.randomUUID(),
+                    title = "Crime #$i",
+                    date = Date(),
+                    isSolved = i % 2 == 0
+                )
 
-            crimes += crime
+                crimes += crime
+            }
         }
     }
 }
